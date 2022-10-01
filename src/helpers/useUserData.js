57be998fea30e10594cb279d80a7b956cab6b213
bocha13 <ignaciobochatay@gmail.com>
2022-10-01 15:12:24 -0300

    added fetching indicators

--- src/helpers/useUserData.js
+++ src/helpers/useUserData.js
@@ -1,7 +1,7 @@
 import { useQuery } from 'react-query'
 export function useUserData(userId) {
-  const usersData = useQuery(['users', userId], () =>
-    fetch(`/api/users/${userId}`).then((res) => res.json())
+  const usersData = useQuery(['users', userId], ({ signal }) =>
+    fetch(`/api/users/${userId}`, { signal }).then((res) => res.json())
   )
 
   return usersData
